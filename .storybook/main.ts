93655636c10f597ec02902e71c5b5d867272cc7c
TomMenga <tommaso.menga-94@hotmail.it>
2023-07-18 10:50:42 +0200

    feat: enable the use of jsx in storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,13 +2,17 @@ import type { StorybookConfig } from "@storybook/web-components-webpack5";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-a11y',
+    '@storybook/addon-interactions',
+  ],
   framework: {
     name: "@storybook/web-components-webpack5",
     options: {},
   },
   docs: {
-    autodocs: "tag",
+    autodocs: true,
   },
   async webpackFinal(config, { configType }) {
     
