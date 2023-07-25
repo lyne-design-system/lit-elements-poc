75a1da83822729bf40aa3c7b1f237016afc1801e
TomMenga <tommaso.menga-94@hotmail.it>
2023-07-25 08:43:13 +0200

    feat(radio-button-group): import component

---
+++ src/sbb-radio-button-group/sbb-radio-button-group.custom.d.ts
@@ -0,0 +1,5 @@
+export interface InterfaceSbbRadioButtonGroupAttributes {
+  size: 's' | 'm';
+  orientation: 'horizontal' | 'vertical';
+  horizontalFrom?: 'zero' | 'micro' | 'small' | 'medium' | 'large' | 'wide' | 'ultra';
+}
